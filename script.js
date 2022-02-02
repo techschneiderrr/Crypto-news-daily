d4a47e6ac0d353b9d2bd9b2b5f14b7b0e5fcd1fd
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-02-03 04:01:23 +0530

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ coin =
 {
     fetchListings : function()
     {
-        fetch("https://crypto-ranking-data.p.rapidapi.com/market/list/1/2", {
+        fetch("https://crypto-ranking-data.p.rapidapi.com/market/list/1/100", {
 	    "method": "GET",
 	    "headers": {
 		"x-rapidapi-host": "crypto-ranking-data.p.rapidapi.com",
@@ -17,13 +17,31 @@ coin =
     },
     displayListings : function(data){
         for(let i=0;i<data.length;i++){
-            var ul = document.querySelector(".list-group");
-            ul.innerHTML('');
-            const {name,symbol,totalSupply,maxSupply,ath,atl,logo} = data[i];
-            
+            const {name,symbol,totalSupply,cmcRank,ath,atl,logo} = data[i];
+            const list = document.createElement("li");
+            list.classList.add("list-group-item");
+            list.insertAdjacentHTML(`afterbegin`,`<div class="outer-flex">
+            <div class="logo">
+                <img src="${logo}" width="">
+            </div>
+            <div class="inner-flex">
+                <div class="cols-flex">
+                    <p><b>Name :</b> ${name}</p>
+                    <p><b>Symbol :</b> ${symbol}</p>
+                </div>
+                <div class="cols-flex extra">
+                    <p><b>Total Supply :</b> ${parseFloat(totalSupply).toFixed(3)} $</p>
+                    <p><b>Rank :</b> ${cmcRank}</p>
+                </div>
+                <div class="cols-flex">
+                    <p><b>All time high :</b> ${parseFloat(ath.toFixed(3))} $</p>
+                    <p><b>All time low :</b> ${parseFloat(atl.toFixed(3))} $</p>
+                </div>
+            </div>
+        </div>`);
+            document.querySelector(".list-group").appendChild(list);
         }
     }
 
 }
-
 coin.fetchListings();
